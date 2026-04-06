c6ee04c1a07f14a99871d9d4a9a4ba786bd975e2
XxDdShotAnas <epicfortniteaass@gmail.com>
2026-04-06 13:43:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+const TOKEN = "MTQ5MDQyNTMxNDA4ODcxODM1Ng.G1nLXj.UwoqQOH5QqGWb7qTFM6cbiarCSPTfcil8YxDmY";
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(TOKEN);
