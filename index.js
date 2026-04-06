a32865913422c65274da966fb29472d85d49084b
XxDdShotAnas <epicfortniteaass@gmail.com>
2026-04-06 14:32:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ process.on('uncaughtException', console.error);
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 const TOKEN = process.env.TOKEN;
 
 client.once('clientReady', () => {
   console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "ping") {
+    message.reply("pong 🏓");
+  }
 });
 
 client.login(TOKEN);
